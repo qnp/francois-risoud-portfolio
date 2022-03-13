6474f91caca94dc59bfe6b7226bda0b8e296e581
François Risoud <francois.risoud@gmail.com>
2022-03-13 21:38:31 +0100

    [code] add new eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  overrides: [
+    {
+      files: ['**/*.vue', '**/*.js'],
+      env: { browser: true },
+      extends: [
+        'plugin:vue/essential',
+        'plugin:vue-pug-sfc/recommended',
+        '@vue/prettier',
+      ],
+      rules: {
+        curly: ['error', 'multi-line'],
+        'vue/multi-word-component-names': 'off',
+        'vue-pug-sfc/component-name-in-template-casing': 'off',
+        'vue-pug-sfc/attribute-hyphenation': 'off',
+        'vue-pug-sfc/require-v-for-key': 'off',
+        'vue-pug-sfc/no-template-key': 'off',
+        'no-console': 'off',
+      },
+    },
+    {
+      files: 'server/**/*.js',
+      env: { node: true },
+      extends: ['plugin:prettier/recommended'],
+      rules: {
+        'prettier/prettier': 'warn',
+      },
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
   ],
-  // add your custom rules here
-  rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    // custom
-    'semi': ['error', 'always'],
-    'comma-dangle': 'off',
-    'padded-blocks': 'off',
-    'space-before-function-paren': 'off',
-    'operator-linebreak': ['error', 'before'],
-    'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 1 }],
-    'space-infix-ops': 'off',
-    'yoda': 'off',
-    'camelcase': 'off'
-  }
-}
+};
